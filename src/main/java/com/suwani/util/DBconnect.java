e650d99b5864884d58e511983bb59792a686af8e
Charith Lakshan <it23841086@my.sliit.lk>
2025-04-20 12:09:40 +0530

    Add DB connection

---
+++ src/main/java/com/suwani/util/DBconnect.java
@@ -0,0 +1,20 @@
+package com.suwani.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnect {
+
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		
+		String uname = "root";
+		String password = "Game@1234";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_management_system?characterEncoding=utf8",uname,password);
+		
+		return con;
+	}
+}
+
